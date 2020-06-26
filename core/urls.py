4b10f65b8a9706c300532f9b162ec0f452d1fc45
Christian Picos <picos.rodriguez.christian@gmail.com>
2020-06-26 15:47:49 -0700

    Getting Real Prices

--- core/urls.py
+++ core/urls.py
@@ -3,23 +3,27 @@ from rest_framework.routers import DefaultRouter
 from .views import AuthorViewSet, TagViewSet, CategoryViewSet, BlogPostViewSet, MacroTrendScrapViewSet, \
     Eps3yCagrViewSet, TestFinance, SltPortfolioViewSet
 
-router = DefaultRouter()
+blogRouter = DefaultRouter()
 # BLOG
-router.register(r'authors', AuthorViewSet)
-router.register(r'tags', TagViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'blog-posts', BlogPostViewSet)
+blogRouter.register(r'authors', AuthorViewSet)
+blogRouter.register(r'tags', TagViewSet)
+blogRouter.register(r'categories', CategoryViewSet)
+blogRouter.register(r'blog-posts', BlogPostViewSet)
 
+adminRouter = DefaultRouter()
 # ADMIN
-router.register(r'macro-scrap', MacroTrendScrapViewSet, basename='macro_trends_scrap') # only admin
-router.register(r'set-eps3y-cagr', Eps3yCagrViewSet, basename='set_eps3y_cagr')  # only admin
+adminRouter.register(r'macro-scrap', MacroTrendScrapViewSet, basename='macro_trends_scrap') # only admin
+adminRouter.register(r'set-eps3y-cagr', Eps3yCagrViewSet, basename='set_eps3y_cagr')  # only admin
 
+portfolioRouter = DefaultRouter()
 # PORTFOLIO
-router.register(r'slt-portfolio', SltPortfolioViewSet, basename='slt_portfolio')  # SanlyTech, Stable Long Term
+portfolioRouter.register(r'slt', SltPortfolioViewSet, basename='slt_portfolio')  # SanlyTech, Stable Long Term
 
 # PYTHON FOR FINANCE BOOK
-router.register(r'test-finance', TestFinance, basename='test_finance')
+portfolioRouter.register(r'test-finance', TestFinance, basename='test_finance')
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("blog/", include(blogRouter.urls)),
+    path("admin/", include(adminRouter.urls)),
+    path("portfolio/", include(portfolioRouter.urls))
 ]
--- core/views.py
+++ core/views.py
@@ -185,7 +185,7 @@ class MacroTrendScrapViewSet(viewsets.ViewSet):
 
                 lines.append(StockValuation(**res_dict))
 
-            StockValuation.objects.bulk_create(lines)
+            # StockValuation.objects.bulk_create(lines)
         return Response(result)
 
 
@@ -212,21 +212,34 @@ class Eps3yCagrViewSet(viewsets.ViewSet):
                 x = StockValuation.objects.get(id=data.get('id'))
                 x.eps3y_cagr = cagr
                 x.eps_start = start_eps
-                x.save()
+                # x.save()
                 print(stock)
         return Response(result)
 
 
 class SltPortfolioViewSet(viewsets.ViewSet):
     def list(self, request):
+        result = self.slt_valuation()
+        result.to_csv(r'C:\Users\Omar\Desktop\SLTPortFolio.csv', index=False)
+        out = result.to_json(orient='records')
+        return Response([])
+
+    @staticmethod
+    def slt_valuation():
         start = datetime.now()
-        # stocks = StockValuation.objects.filter(roe__gte=15, roi__gte=10, roa__gte=5, net_profit_margin__gte=10,
-        #                                        current_ratio__gte=1.2, pe_ratio__lte=25)\
-        #          .order_by('stock__ticker', 'date')
-        stocks = StockValuation.objects.all().values('stock__ticker', 'date', 'roe', 'roi', 'roa', 'net_profit_margin',
-                                                     'gross_margin', 'operating_margin', 'ebitda_margin',
-                                                     'current_ratio', 'debt_to_equity', 'eps_ttm', 'pe_ratio',
-                                                     'eps3y_cagr', 'eps_start', 'price')
+        stocks = StockValuation.objects.filter(roe__gte=15, roi__gte=10, roa__gte=5, net_profit_margin__gte=10,
+                                               current_ratio__gte=1.2, pe_ratio__lte=25).values('stock__ticker', 'date',
+                                                                                                'roe', 'roi', 'roa',
+                                                                                                'net_profit_margin',
+                                                                                                'gross_margin',
+                                                                                                'operating_margin',
+                                                                                                'ebitda_margin',
+                                                                                                'current_ratio',
+                                                                                                'debt_to_equity',
+                                                                                                'eps_ttm', 'pe_ratio',
+                                                                                                'eps3y_cagr',
+                                                                                                'eps_start', 'price') \
+            .order_by('stock__ticker', 'date')
         df = pd.DataFrame(stocks)
         # CONDITIONS, FILTERS df[df['columns'] = condition ]
         df_gt25 = df[df['eps3y_cagr'] > 25]
@@ -245,17 +258,11 @@ class SltPortfolioViewSet(viewsets.ViewSet):
         result["date"] = pd.to_datetime(result["date"]).dt.strftime("%Y-%m-%d")
 
         # VALUATION FILTER
-        result = result[result['pe_ratio'] < result['eps3y_cagr']]
-
-        # result.to_csv(r'C:\Users\Omar\Desktop\SLTPortFolio.csv', index=False)
-        # rule sell after 1 year holding for the last positition
-
-        # FUNCTIONS
-        print(result.describe())
+        result = result[(result['pe_ratio'] < result['eps3y_cagr']) & (result['mof'] >= 30)]
         end = datetime.now()
         print('ELAPSED TIME', end - start)
-        out = result.to_json(orient='records')
-        return Response([])
+        # rule sell after 1 year holding for the last position
+        return result
 
 
 class GenerateScrap(TemplateView):
@@ -266,43 +273,20 @@ class TestFinance(viewsets.ViewSet):
 
     def list(self, request):
         result = []
-        # tickers = Stock.objects.all().values_list('ticker', flat=True)[:5]
-        # start = '2000-01-01'
-        # end = '2020-12-31'
-        # df = pd.concat([web.DataReader('BIIB', 'yahoo', start, end) for ticker in tickers]).reset_index()
-        # print(df)
-        # https://www.nasdaq.com/market-activity/stocks/screener?exchange=nasdaq&letter=0&render=download
-        # https://www.nasdaq.com/market-activity/stocks/screener?exchange=nyse&letter=0&render=download
-        # ftp://ftp.nasdaqtrader.com/symboldirectory
-        # https://pypi.org/project/Yahoo-ticker-downloader/
+        frame = SltPortfolioViewSet.slt_valuation()
 
-        # df = pd.concat([web.DataReader(ticker,'morningstar', start, end) for ticker in tickers]).reset_index()
-        # ts_log = np.round(sp500['Close'])
-        # sp500['42d'] = ts_log.rolling(42).mean()
-        # sp500['252d'] = ts_log.rolling(252).mean()
-        # sp500['42d-252d'] = sp500['42d'] - sp500['252d']
+        for index, row in frame.tail().iterrows():
+            price_date = datetime.strptime(row['date'], '%Y-%m-%d')
+            if price_date.weekday() not in [0, 1, 2, 3, 4]:
+                if price_date.weekday() == 5:
+                    price_date = price_date - relativedelta(days=1)
+                elif price_date.weekday() == 6:
+                    price_date = price_date - relativedelta(days=2)
 
-        # GOOD ONLY FOR INDEXES
-        # SD = 50
-        # sp500['Regime'] = np.where(sp500['42d-252d'] > SD, 1, 0)
-        # sp500['Regime'] = np.where(sp500['42d-252d'] < SD, -1, sp500['Regime'])
-        # sp500['Regime'].value_counts()
-        # sp500['Market'] = np.log(sp500['Close'] / sp500['Close'].shift(1))
-        # sp500['Strategy'] = sp500['Regime'].shift(1) * sp500['Market']
-        #
-        i = 0
-        for stock in Stock.objects.all().values_list('ticker', flat=True):
-            if stock not in ['BHGE']:
-                try:
-                    sp500 = web.DataReader(stock, data_source='yahoo', start='2000-01-01', end='2020-12-31')
-                    sp500['Symbol'] = stock
-                    # print(stock)
-                    # print(sp500[['Adj Close', 'Close', 'Symbol']])
-                except Exception as err:
-                    print(stock, err)
-            i += 1
-            if i == 30:
-                time.sleep(30)
-                i = 0
+            prices = web.DataReader(row['stock__ticker'], data_source='yahoo',
+                                    start=price_date.date(), end=price_date.date())
+            frame.at[index, 'Real Price'] = round(prices.iloc[0]['Close'], 2)
+
+        print(frame.tail())
         print('END !!!!!')
         return Response(result)
